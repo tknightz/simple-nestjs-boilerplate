77ff547fc0def05e46445238b1d8815e80eecad6
tknightz <hatuyen2699@gmail.com>
2025-04-30 23:06:23 +0700

    feat: replace eslint and prettier with biome, npm with bun

--- src/auth/dto/auth-login.dto.ts
+++ src/auth/dto/auth-login.dto.ts
@@ -1,6 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty } from 'class-validator';
-import { Transform } from 'class-transformer';
 
 export class AuthLoginDto {
   @ApiProperty({ example: 'test1@example.com' })
