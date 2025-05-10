59feda7ece93ce9f680d8d2353aa4108b62350a7
tknightz <hatuyen2699@gmail.com>
2025-05-10 22:02:49 +0700

    fix: schema import path

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { User, UserDocument } from '../database/schema/user.schema';
+import { User, UserDocument } from '../database/schemas/user.schema';
 import { usersSeed } from './data/users.seed';
 
 @Injectable()
